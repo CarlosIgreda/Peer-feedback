878a4e5dae72d7a90f07c6049e56cd03f94e62f9
CarlosIgreda <cjigredarivas@gmail.com>
2023-04-11 10:29:30 -0500

    Update functionality.js

--- save.js
+++ save.js
@@ -1,6 +1,6 @@
 // Define a function to save the tasks in local storage
 const saveTasks = (tasks) => {
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-  };
-  
-  export default saveTasks;
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+
+export default saveTasks;
